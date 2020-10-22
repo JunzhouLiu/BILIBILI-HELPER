b46e5305121247d6653cd46582eb73cb7d4be13c
zqrren <zqrren@outlook.com>
2020-10-22 23:49:28 +0800

    修改任务顺序，先直播签到再换硬币，避免出现银瓜子不够无法兑换硬币但直播签到后却够了的情况

--- src/main/java/top/misec/task/DailyTask.java
+++ src/main/java/top/misec/task/DailyTask.java
@@ -501,9 +501,9 @@ public class DailyTask {
         wechatLogger.info(Config.getInstance().outputConfig());
         videoWatch();//观看视频 默认会调用分享
         doMangaSign();//漫画签到
+        doLiveCheckin(); //直播签到
         silver2coin();//银瓜子换硬币
         doCoinAdd();//投币任务
-        doLiveCheckin(); //直播签到
         doCharge();
         mangaGetVipReward(1);
         logger.info("本日任务已全部执行完毕");
