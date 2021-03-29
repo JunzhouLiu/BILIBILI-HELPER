47d25fd7041bdbb0517e897064a35e739a80c751
wcrlan <32435032+wcrlan@users.noreply.github.com>
2021-03-29 10:59:02 +0800

    Update Config.java

--- src/main/java/top/misec/config/Config.java
+++ src/main/java/top/misec/config/Config.java
@@ -91,7 +91,9 @@ public class Config {
 
         Config.CONFIG = new Gson().fromJson(configJson, Config.class);
         List<String> uas = Config.getInstance().getUserAgent();
-        HttpUtil.setUserAgent(uas.get(new Random().nextInt(uas.size())));
+        String ua = uas.get(new Random().nextInt(uas.size()));
+        HttpUtil.setUserAgent(ua);
         logger.info(Config.getInstance().toString());
+        logger.info("选择的UA: {}", ua);
     }
 }
